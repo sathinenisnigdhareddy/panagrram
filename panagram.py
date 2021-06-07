0f59663f33528591c132a00675f68acef760121e
sathinenisnigdhareddy <66218120+sathinenisnigdhareddy@users.noreply.github.com>
2021-06-07 15:31:38 +0530

    Update panagram.py

--- panagram.py
+++ panagram.py
@@ -1,4 +1,4 @@
-#panagram program
+#panagram program in python
 
 capital=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 small=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
